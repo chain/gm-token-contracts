d76f794f4e9ee7c6746437bc9ca1303111ff7d84
reptcef <rept@mailtime.com>
2022-11-24 12:39:20 +0800

    add XCNTokenExchange migration test

--- truffle/test/upgradeable_test.js
+++ truffle/test/upgradeable_test.js
@@ -26,8 +26,6 @@ contract('Test cases for upgradeable contracts', (accounts) => {
     const rate = 2; // pre-defined in V2 token exchange contract
 
     let contractInstances;
-    let mdtExchangeContract2;
-
 
     beforeEach('Basic setup', async () => {
         contractInstances = await environment.contractInstances.loadAll();
@@ -62,7 +60,7 @@ contract('Test cases for upgradeable contracts', (accounts) => {
 
     it('Should be able to upgrade MDT exchange contract', async () => {
         try {
-            mdtExchangeContract2 = await upgradeProxy(contractInstances.mdtExchangeContract, MDTExchangeV2);
+            await upgradeProxy(contractInstances.mdtExchangeContract, MDTExchangeV2);
             console.log('Proxy', contractInstances.mdtExchangeContract.address, 'has upgraded its implementation');
         } catch (ex) {
             assert.ok(false, 'The transaction upgrades implementation is reverted');
@@ -82,6 +80,73 @@ contract('Test cases for upgradeable contracts', (accounts) => {
         assert.equal(Number(userBalancesBefore.MDT) - Number(userBalancesAfter.MDT), Math.pow(10, 18) * amount, 'User should loss ' + amount + ' XCN');
 
         assert.equal(Number(userBalancesAfter.GM) - Number(userBalancesBefore.GM), Math.pow(10, 18) * amount * rate, 'User should earn ' + amount * rate + ' GM');
+    });
+
+    it('Burning GM should be exchanged for equivalent XCN', async () => {
+        let userBalancesBefore = await checkBalance(user);
+        let contractBalancesBefore = await checkBalance(contractInstances.xcnExchangeContract.address);
+
+        let amount = 10;
+        await exchangeGmForXcn(user, amount);
+        console.log(user, 'send', amount, 'GM to', contractInstances.xcnExchangeContract.address, 'to exchange XCN');
+        let userBalancesAfter = await checkBalance(user);
+        let contractBalancesAfter = await checkBalance(contractInstances.xcnExchangeContract.address);
+
+        assert.equal(Number(userBalancesBefore.GM) - Number(userBalancesAfter.GM), Math.pow(10, 18) * amount, 'User should loss ' + amount + ' GM');
+
+        assert.equal(Number(userBalancesAfter.XCN) - Number(userBalancesBefore.XCN), Math.pow(10, 18) * amount, 'User should earn ' + amount + ' XCN');
+
+        assert.equal(Number(contractBalancesAfter.GM) - Number(contractBalancesBefore.GM), 0, 'Contract should not have any changes on GM balance');
+    });
+
+    it('Depositing XCN to Exchange contract should mint equivalent amount of GM', async () => {
+        let userBalancesBefore = await checkBalance(user);
+        let contractBalancesBefore = await checkBalance(contractInstances.xcnExchangeContract.address);
+
+        let amount = 5;
+        await exchangeXcnForGm(user, amount);
+        console.log(user, 'send', amount, 'XCN to', contractInstances.xcnExchangeContract.address, 'to exchange GM');
+        let userBalancesAfter = await checkBalance(user);
+        let contractBalancesAfter = await checkBalance(contractInstances.xcnExchangeContract.address);
+
+        assert.equal(Number(userBalancesBefore.XCN) - Number(userBalancesAfter.XCN), Math.pow(10, 18) * amount, 'User should loss ' + amount + ' XCN');
+
+        assert.equal(Number(userBalancesAfter.GM) - Number(userBalancesBefore.GM), Math.pow(10, 18) * amount, 'User should earn ' + amount + ' GM');
+
+        assert.equal(Number(contractBalancesAfter.GM) - Number(contractBalancesBefore.GM), 0, 'Contract should not have any changes on GM balance');
+    });
+
+    it('Should be able to upgrade XCN exchange contract', async () => {
+        try {
+            await upgradeProxy(contractInstances.xcnExchangeContract, XCNExchangeV2);
+            console.log('Proxy', contractInstances.xcnExchangeContract.address, 'has upgraded its implementation');
+        } catch (ex) {
+            assert.ok(false, 'The transaction upgrades implementation is reverted');
+        }
+    });
+
+    it('Burning GM should be exchanged for equivalent XCN', async () => {
+
+        let amount = 10;
+        await truffleAssert.reverts(exchangeGmForXcn(user, amount),
+            'XCNTokenExchangeV2: GM cannot be used for exchanging anymore');
+    });
+
+    it('Depositing XCN to Exchange contract should mint equivalent amount of GM', async () => {
+        let userBalancesBefore = await checkBalance(user);
+        let contractBalancesBefore = await checkBalance(contractInstances.xcnExchangeContract.address);
+
+        let amount = 5;
+        await exchangeXcnForGm(user, amount);
+        console.log(user, 'send', amount, 'XCN to', contractInstances.xcnExchangeContract.address, 'to exchange GM');
+        let userBalancesAfter = await checkBalance(user);
+        let contractBalancesAfter = await checkBalance(contractInstances.xcnExchangeContract.address);
+
+        assert.equal(Number(userBalancesBefore.XCN) - Number(userBalancesAfter.XCN), Math.pow(10, 18) * amount, 'User should loss ' + amount + ' XCN');
+
+        assert.equal(Number(userBalancesAfter.GM) - Number(userBalancesBefore.GM), Math.pow(10, 18) * amount * rate, 'User should earn ' + amount * rate + ' GM');
+
+        assert.equal(Number(contractBalancesAfter.GM) - Number(contractBalancesBefore.GM), 0, 'Contract should not have any changes on GM balance');
     });
 
 });
